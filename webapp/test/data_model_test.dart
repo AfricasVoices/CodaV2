07e4eb59a3a96c66066cb006739c8fa31cc8c4a4
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2018-09-24 14:17:49 +0100

    Add doc comments, colour class and check and rename files as per PR

--- webapp/test/data_model_test.dart
+++ webapp/test/data_model_test.dart
@@ -2,53 +2,55 @@ import 'dart:convert' show json;
 
 import 'package:test/test.dart';
 
-import 'package:CodaV2/datastore.dart' as ds;
+import 'package:CodaV2/data_model.dart';
 
 void main() {
   test("Empty dataset", () {
-    ds.Dataset dataset = new ds.Dataset('Name');
+    Dataset dataset = new Dataset('Name');
     expect(dataset.messages, []);
     expect(dataset.codeSchemes, []);
   });
 
   test("Simple dataset from JSON", () {
-    Map datasetMap = json.decode(testJsonCollection);
-    ds.Dataset dataset = new ds.Dataset.fromJson(datasetMap);
+    Map datasetMap = json.decode(testJsonDataset);
+    Dataset dataset = new Dataset.fromJson(datasetMap);
 
     expect(dataset.messages.length, 2);
     expect(dataset.codeSchemes.length, 1);
-    expect(dataset.messages[0].messageID, "1");
+    expect(dataset.messages[0].messageID, "msg 1");
     expect(dataset.codeSchemes[0].schemeID, "scheme 1");
+    expect(dataset.codeSchemes[0].codes[0]['colour'], new Colour.hex('f46241'));
+    expect(dataset.codeSchemes[0].codes[1]['colour'], new Colour());
   });
 }
 
-String testJsonCollection =
+const String testJsonDataset =
 """{
   "Name": "Test",
   "Documents": [
     {
-      "MessageID": "1",
+      "MessageID": "msg 1",
       "Text": "first message",
       "CreationDateTimeUTC": "2018-09-23T14:12:00Z",
       "Labels": [
         {
           "SchemeID": "scheme 1",
           "DateTimeUTC": "2018-09-23T14:12:00Z",
-          "ValueID": "1",
-          "LabelOrigin": "manual"
+          "ValueID": "code1",
+          "LabelOrigin": "info@example.com"
         }
       ]
     },
     {
-      "MessageID": "2",
+      "MessageID": "msg 2",
       "Text": "second message",
       "CreationDateTimeUTC": "2018-09-23T14:14:00Z",
       "Labels": [
         {
           "SchemeID": "scheme 1",
           "DateTimeUTC": "2018-09-23T14:12:00Z",
-          "ValueID": "2",
-          "LabelOrigin": "manual"
+          "ValueID": "code 2",
+          "LabelOrigin": "info@example.com"
         }
       ]
     }
@@ -59,13 +61,12 @@ String testJsonCollection =
       "Codes": [
         {
           "FriendlyName": "code 1",
-          "ValueID": "1",
-          "Colour": "red"
+          "ValueID": "code 1",
+          "Colour": "#f46241"
         },
         {
           "FriendlyName": "code 2",
-          "ValueID": "2",
-          "Colour": "blue"
+          "ValueID": "code 2"
         }
       ]
     }
