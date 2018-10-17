b51dfdb761305fe253fa17741ad41e3a0db5ebc8
Luke Church <luke@church.name>
2018-10-17 21:11:24 +0100

    Add get dataset ids

---
+++ data_tools/get_dataset_ids.py
@@ -0,0 +1,27 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+import validate_code_scheme
+
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description="push data to firestore")
+parser.add_argument("crypto_token_path", help="Path to crypto key", nargs=1)
+
+args = parser.parse_args()
+CRYPTO_TOKEN_PATH = args.crypto_token_path[0]
+
+# Use a service account
+# Setup Firebase
+cred = credentials.Certificate(CRYPTO_TOKEN_PATH)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+datasets = db.collection(u'datasets').get()
+
+print ("Existing dataset ids:")
+for dataset in datasets:
+    print(u'\t{}'.format(dataset.id))
