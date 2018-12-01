972a94ea84f727b864fee5c93e1d9a9dbc9ab5f1
Luke Chruch <luke@church.name>
2018-12-01 15:20:00 +0000

    Add tool to duplicate a code scheme

---
+++ data_tools/duplicate_scheme.py
@@ -0,0 +1,44 @@
+import firebase_client_wrapper as fcw
+
+import json
+import sys
+
+import validate_code_scheme
+import validate_message_structure
+import validate_user_list
+
+if (len(sys.argv) != 4):
+    print ("Usage python duolicate.py crypto_token dataset_id scheme_id")
+    print ("Adds another duplication of the scheme if provided")
+    exit(1)
+
+CRYPTO_TOKEN_PATH = sys.argv[1]
+fcw.init_client(CRYPTO_TOKEN_PATH)
+
+DATASET_ID = sys.argv[2]
+SCHEME_ID = sys.argv[3]
+
+dataset_ids = fcw.get_dataset_ids()
+
+if DATASET_ID not in dataset_ids: 
+    print ("Dataset {} does not exist".format(DATASET_ID))
+    exit (1)
+
+scheme_ids = fcw.get_code_scheme_ids(DATASET_ID)
+if SCHEME_ID not in scheme_ids:
+    print ("Scheme {} does not exist".format(SCHEME_ID))
+    exit (1)
+
+exitsting_scheme_duplicates = [scheme for scheme in scheme_ids if scheme.startswith(SCHEME_ID)]
+
+existing_scheme_count = len(exitsting_scheme_duplicates)
+assert existing_scheme_count > 0
+
+next_scheme_id = existing_scheme_count # Schemes are zero indexded
+scheme = fcw.get_code_scheme(DATASET_ID, SCHEME_ID)
+new_scheme_id = SCHEME_ID + "-{}".format(next_scheme_id)
+scheme["SchemeID"] = new_scheme_id
+fcw.get_code_scheme_ref(DATASET_ID, new_scheme_id).set(scheme)
+
+print ("Added: {}".format(new_scheme_id))
+
