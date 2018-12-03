79b4b296d89fe63bc13f8f62dee1be7af95a206e
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2018-12-03 21:34:32 +0000

    Add deploy template for wts

---
+++ webapp/lib/firebase_constants_wts.dart
@@ -0,0 +1,9 @@
+library firebase.constants;
+
+const String apiKey = "AIzaSyAdwgBTgdD2oNYP9VwyS9fQd6sf5roqLuA";
+const String authDomain = "web-coda.firebaseapp.com";
+const String databaseURL = "https://web-coda.firebaseio.com";
+const String projectId = "web-coda";
+const String storageBucket = "web-coda.appspot.com";
+const String messagingSenderId = "329672882311";
+const String logUrl = "https://us-central1-web-coda.cloudfunctions.net/Log";
