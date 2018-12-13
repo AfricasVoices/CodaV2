b24af2836eb3c194a42eb3f7a08c5c7a1407773b
Isaack Mwenda <mwendaisaac@gmail.com>
2018-12-13 13:06:06 +0300

    Added a func that fetches messages,computes coding progress and pushes it to the metrics collection.

--- data_tools/compute_coding_progress.py
+++ data_tools/compute_coding_progress.py
@@ -6,36 +6,37 @@ from time import gmtime, strftime
 
 if (len(sys.argv) != 2):
     print ("Usage python compute_coding_progress.py coda_crypto_token")
-    exit(1)
+        exit(1)
 
 CODA_CRYPTO_TOKEN_PATH = sys.argv[1]
-
 fcw.init_client(CODA_CRYPTO_TOKEN_PATH)
-data = {}
 
-ids = fcw.get_dataset_ids()
-
-data['coding_progress'] = {}
-
-for id in ids:
-    data['coding_progress'][id] = {}
-    messages = []
-    messages_with_labels = 0
-
-    # New scheme
-    metrics = fcw.get_dataset_metrics(id)
-    if metrics != None:
-        data['coding_progress'][id] = metrics   
-        continue
-        
+def compute_coding_progress(ids,messages,metrics):
+    data = {}
+    data['coding_progress'] = {}
+    for id in ids:
+        data['coding_progress'][id] = {}  
+        messages_with_labels = 0
+        # New scheme
+        if metrics != None:
+            data['coding_progress'][id] = metrics   
+            continue 
+            if len(message["Labels"]) > 0:
+                messages_with_labels += 1
+        data['coding_progress'][id]['messages_count'] = len(messages)
+        data['coding_progress'][id]['messages_with_label'] = messages_with_labels
+    data["last_update"] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    return json.dumps(data, indent=2)
+    
+def get_messages_ids_metrics(): 
+    global ids = fcw.get_dataset_ids()
+    global metrics = fcw.get_dataset_metrics(id)
+    global messages = []
     for message in fcw.get_all_messages(id):
         messages.append(message)
-        if len(message["Labels"]) > 0:
-            messages_with_labels += 1
+    compute_coding_progress()
 
-    data['coding_progress'][id]['messages_count'] = len(messages)
-    data['coding_progress'][id]['messages_with_label'] = messages_with_labels
 
-data["last_update"] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-print (json.dumps(data, indent=2))
+
+    
