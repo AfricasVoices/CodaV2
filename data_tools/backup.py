b7bfa531f8a79c3085b84277a795268ab1597521
Luke Chruch <luke@church.name>
2018-12-24 21:45:58 +0000

    backup specific project

--- data_tools/backup.py
+++ data_tools/backup.py
@@ -3,16 +3,19 @@ import firebase_client_wrapper as fcw
 import json
 import sys
 
-if (len(sys.argv) != 2):
-    print ("Usage python backup.py crypto_token")
+if (len(sys.argv) < 2 or len(sys.argv) > 3):
+    print ("Usage python backup.py crypto_token [dataset_id]")
     exit(1)
 
 CRYPTO_TOKEN_PATH = sys.argv[1]
 fcw.init_client(CRYPTO_TOKEN_PATH)
 
-data = {}
+if len(sys.argv) == 3:
+    ids = [sys.argv[2]]
+else:
+    ids = fcw.get_dataset_ids()
 
-ids = fcw.get_dataset_ids()
+data = {}
 
 for id in ids:
     data[id] = {}
