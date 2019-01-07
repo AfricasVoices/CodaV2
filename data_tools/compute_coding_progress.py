4dd662b16f0ca806f2c1057fae0e9b811b706612
Luke Chruch <luke@church.name>
2019-01-07 09:18:38 +0000

    Address review comments

--- data_tools/compute_coding_progress.py
+++ data_tools/compute_coding_progress.py
@@ -5,6 +5,9 @@ import sys
 from time import gmtime, strftime
 
 def compute_coding_progress(id):
+    """Compute and return the progress metrics for a given dataset.
+    This method will initialise the counts in Firestore if they do
+    not already exist."""
     messages = []
     messages_with_labels = 0
 
@@ -26,26 +29,19 @@ def compute_coding_progress(id):
     fcw.set_dataset_metrics(id, metrics)
     return metrics
 
-
 if __name__ == "__main__":
     if (len(sys.argv) != 2):
         print ("Usage python compute_coding_progress.py coda_crypto_token")
         exit(1)
 
     CODA_CRYPTO_TOKEN_PATH = sys.argv[1]
-
     fcw.init_client(CODA_CRYPTO_TOKEN_PATH)
+
     data = {}
-
     ids = fcw.get_dataset_ids()
-
     data['coding_progress'] = {}
-
     for id in ids:
         data['coding_progress'][id] = compute_coding_progress(id)
 
     data["last_update"] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
     print (json.dumps(data, indent=2))
-
-
