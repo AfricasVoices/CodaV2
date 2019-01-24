b1fd8eace6a88f6493a02a82e7cbc0d50ad61192
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2019-01-24 18:04:05 +0000

    Make the mini python script in bash compatible with both python3 and 2

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -21,7 +21,7 @@ cd ..
 cp $PATH_TO_CONSTANTS_FILE public/assets/firebase_constants.json
 
 # Get the project id
-PROJECT_ID=$(cat $PATH_TO_CONSTANTS_FILE | python -c 'import json,sys; constants=json.load(sys.stdin); print constants["projectId"]')
+PROJECT_ID=$(cat $PATH_TO_CONSTANTS_FILE | python -c 'import json,sys; constants=json.load(sys.stdin); print(constants["projectId"])')
 
 # Deploy
 firebase deploy --project $PROJECT_ID --public public
