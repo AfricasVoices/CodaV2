aee78e3bc40a0fa68ebc4687e868d267e2a5cee4
Luke Chruch <luke@church.name>
2019-02-05 18:24:06 +0000

    Temp disable reporting

--- ml_tools/build_and_test.py
+++ ml_tools/build_and_test.py
@@ -20,7 +20,7 @@ parser.add_argument("labelled_data_path", help="Path to labelled data file", nar
 # parser.add_argument("model_output_path", help="Path to cache the model to", nargs=1)
 
 args = parser.parse_args()
-REPORT_PARTIAL_SCORES = 50
+REPORT_PARTIAL_SCORES = 0
 REPEATS = 1
 # OUTPUT_PATH = args.output_path
 LABELLED_DATA_PATH = args.labelled_data_path[0]
@@ -29,7 +29,7 @@ LABELLED_DATA_PATH = args.labelled_data_path[0]
 with open(LABELLED_DATA_PATH, "r") as labelled_data_file:
     labelled_data = json.load(labelled_data_file)
 
-log("Loaded data")
+log(f"Loaded data: {LABELLED_DATA_PATH}")
 
 all_scores = []
 model = None
@@ -57,34 +57,42 @@ for repeat in range(REPEATS):
         assert len(messages) == len(labels)
         assert len(messages) > 0 and len(labels) > 0
 
-        if (REPORT_PARTIAL_SCORES
-            and i >= MINIMUM_MESSAGES
-            and len(seen_labels) > 1             # Ensures multiple labels available for classification
-            and i % REPORT_PARTIAL_SCORES == 0):
+        # if (REPORT_PARTIAL_SCORES
+        #     and i >= MINIMUM_MESSAGES
+        #     and len(seen_labels) > 1             # Ensures multiple labels available for classification
+        #     and i % REPORT_PARTIAL_SCORES == 0):
 
-            log("{} messages added".format(i))
-            model, scores = model_utils.build_and_evaluate(messages, labels)
+        #     log("{} messages added".format(i))
+        #     model, scores = model_utils.build_and_evaluate(messages, labels)
 
-            for score in scores:
-                score["training-size"] = len(messages)
+        #     for score in scores:
+        #         score["training-size"] = len(messages)
 
-            single_run_scores.extend(scores)
+        #     single_run_scores.extend(scores)
 
-    assert len(messages) == len(labels)
+    # assert len(messages) == len(labels)
     assert len(messages) > 1 and len(labels) > 1
     assert len(seen_labels) > 1
 
     model, scores = model_utils.build_and_evaluate(messages, labels)
     log("Model built")
 
-    for score in scores:
-        score["training-size"] = len(messages)
-    single_run_scores.extend(scores)
+    for model_scores in scores:
+        log (f"{model_scores}")
+        for code_id in model_scores:
+            scores = model_scores[code_id]
+            log (f"{code_id} :\t {scores['precision']} {scores['recall']} {scores['support']}")
 
-    for score_set in single_run_scores:
-        score_set["run"] = repeat
+    # for score in scores:
+        # score["training-size"] = len(messages)
+    # single_run_scores.extend(scores)
 
-    all_scores.extend(single_run_scores)
+    # for score_set in single_run_scores:
+        # score_set["run"] = repeat
+ 
+    # all_scores.extend(single_run_scores)
+
+
 
 # with open(MODEL_OUTPUT_PATH, 'wb') as model_file:
 #     pickle.dump(model, model_file)
