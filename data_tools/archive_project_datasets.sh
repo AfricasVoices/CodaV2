21792447b1b7ee4102dd8a1378738429318b1d83
Mariana Mărășoiu <mariana.marasoiu@gmail.com>
2020-03-11 08:42:07 +0000

    Rename transfer-temp to archive-folder

--- data_tools/archive_project_datasets.sh
+++ data_tools/archive_project_datasets.sh
@@ -2,20 +2,20 @@ set -e
 
 # Check that the correct number of arguments were provided.
 if [ $# -ne 3 ]; then
-    echo "Usage: ./archive_dataset.sh <crypto-token> <transfer-temp> <project-filter>"
+    echo "Usage: ./archive_dataset.sh <crypto-token> <archive-folder> <project-filter>"
     exit
 fi
 
 # Assign the program arguments to bash variables.
 CRYPTO_TOKEN=$1
-TRANFER_TEMP=$2
+ARCHIVE_FOLDER=$2
 PROJECT_FILTER=$3
 
-PROJECT_IDS=$(python get_dataset_ids.py "$1" | grep "$3")
+PROJECT_IDS=$(python get_dataset_ids.py "$CRYPTO_TOKEN" | grep "$PROJECT_FILTER")
 
 for DATASET_ID in $PROJECT_IDS
 do
     echo $DATASET_ID
-    python backup.py "$1" "$DATASET_ID" > "$2/$DATASET_ID.json" 
-    gzip -f "$2/$DATASET_ID.json"
+    python backup.py "$CRYPTO_TOKEN" "$DATASET_ID" > "$ARCHIVE_FOLDER/$DATASET_ID.json" 
+    gzip -f "$ARCHIVE_FOLDER/$DATASET_ID.json"
 done
