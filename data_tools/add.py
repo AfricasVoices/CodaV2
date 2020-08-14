c47977d03f96ba3e97c704c967c755f0f8b666cb
Alexander Simpson <as2388@cam.ac.uk>
2020-08-14 15:20:37 +0100

    Support incremental updates in add.py

--- data_tools/add.py
+++ data_tools/add.py
@@ -1,5 +1,8 @@
 import argparse
 import json
+import warnings
+
+from dateutil.parser import isoparse
 
 import firebase_client_wrapper as fcw
 import validate_code_scheme
@@ -7,7 +10,10 @@ import validate_message_structure
 
 parser = argparse.ArgumentParser(description="Adds data items that don't currently exist, by id. "
                                              "Existing data items are untouched, even if they differ in content.")
-
+parser.add_argument("--previous-export-file-path",
+                    help="Path to a previous export of messages data. If provided, only messages changed since this "
+                         "export was created will be downloaded from Firestore, and the output data will be the "
+                         "contents of the previous export, updated with the latest data from Firestore")
 parser.add_argument("firestore_credentials_file_path", metavar="firestore-credentials-file-path",
                     help="Path to the Firestore credentials file")
 parser.add_argument("dataset_id", metavar="dataset-id", help="Id of dataset to add data to")
@@ -16,10 +22,14 @@ parser.add_argument("path", help="Path to a JSON file containing the data to add
 
 args = parser.parse_args()
 
+previous_export_file_path = args.previous_export_file_path
 firestore_credentials_file_path = args.firestore_credentials_file_path
 dataset_id = args.dataset_id
 content_type = args.content_type
 path = args.path
+
+assert previous_export_file_path is None or content_type != "schemes", \
+    "Cannot use previous-export-file-path with content-type 'schemes'"
 
 fcw.init_client(firestore_credentials_file_path)
 
@@ -58,9 +68,26 @@ elif content_type == "schemes":
     
     print(f"Added: {added}, Skipped: {skipped_existing}")
 elif content_type == "messages":
+    previous_export = []
+    last_updated = None
+    if previous_export_file_path is not None:
+        with open(previous_export_file_path) as f:
+            previous_export = json.load(f)
+        for msg in previous_export:
+            if "LastUpdated" in msg and (last_updated is None or isoparse(msg["LastUpdated"]) > last_updated):
+                last_updated = isoparse(msg["LastUpdated"])
+        if last_updated is None:
+            warnings.warn(f"Previous export file {previous_export_file_path} does not contain a message with a "
+                          f"'LastUpdated' field; performing a full download of the entire dataset...")
+
+    messages_dict = {msg["MessageID"]: msg for msg in previous_export}
+    new_messages_dict = {msg["MessageID"]: msg for msg in
+                         fcw.get_all_messages(dataset_id, last_updated_after=last_updated)}
+    messages_dict.update(new_messages_dict)
+    all_messages = list(messages_dict.values())
+
     added = 0
     skipped_existing = 0
-    all_messages = fcw.get_all_messages(dataset_id)
 
     existing_ids = set()
     highest_seq_no = -1
